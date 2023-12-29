5cde8b8cd1d334e9b34138c5642842c66b65d5d8
KantapongWongJC14405427 <128341706+KantapongWongJC14405427@users.noreply.github.com>
2023-12-29 15:05:51 +0800

    Version 1

---
+++ newtheme/front-page.php
@@ -0,0 +1,19 @@
+<?php get_header();?>
+
+
+    Test
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Left Side
+            </div>
+
+            <div class="col">
+                Right Side
+            </div>
+        </div>
+        
+    </div>
+
+<?php get_footer();?>
