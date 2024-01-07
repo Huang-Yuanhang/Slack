eb1516a747c6d40c5b9e8ca9801c25532458c356
KantapongWongJC14405427 <128341706+KantapongWongJC14405427@users.noreply.github.com>
2024-01-08 01:11:53 +0800

    editted #site-branding

--- version 3/header.php
+++ version 3/header.php
@@ -56,7 +56,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'version2' ); ?></button>
+			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'version2' ); ?></button> -->
 			<?php
 			wp_nav_menu(
 				array(
