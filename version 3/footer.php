d463983d1bc85d4c74fe8b9c67afe80d53a00c1a
KantapongWongJC14405427 <128341706+KantapongWongJC14405427@users.noreply.github.com>
2024-01-08 01:10:37 +0800

    Updated Footer items

--- version 3/footer.php
+++ version 3/footer.php
@@ -10,46 +10,45 @@
  */
 
 ?>
+	<footer id="colophon" class="site-footer">
 
 	<footer id="colophon" class="site-footer">
+		<div class="footer-content">
+			<!-- JCU Information -->
+			<div class="jcu-info">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/logo.png" alt="James Cook University Logo" class="footer-logo">
+				</a>
+				<p>James Cook University is renowned for its excellence in teaching and research, focusing on courses and programs relevant to the tropics.</p>
+			</div>
+
+			<!-- Footer Navigation -->
+			<nav class="footer-navigation">
+				<ul id="primary-menu" class="menu">
+					<li class="menu-item"><a href="<?php echo home_url('/'); ?>">Home</a></li>
+					<li class="menu-item"><a href="<?php echo home_url('#about'); ?>">About</a></li>
+					<li class="menu-item"><a href="<?php echo home_url('#enroll-now-form'); ?>">Enroll Now</a></li>
+				</ul>
+			</nav>
+
+			<!-- WordPress and Theme Info -->
+			<!-- <div class="wp-theme-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'version2' ) ); ?>">
+					<?php printf( esc_html__( 'Proudly powered by %s', 'version2' ), 'WordPress' ); ?>
+				</a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'version2' ), 'version2', '<a href="http://underscores.me/">Underscores.me</a>' ); ?>
+			</div> -->
+		</div><!-- .footer-content -->
+
 		<div class="site-info">
-
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/logo.png" alt="James Cook University Logo" class="footer-logo">
-            </a>
-            <nav class="footer-navigation">
-                <!-- Footer navigation items -->
-            </nav>
-
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'version2' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'version2' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'version2' ), 'version2', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>&copy; <?php echo date("Y"); ?> James Cook University. All rights reserved.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
-<script>
-// Add smooth scrolling for all links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-</script>
 
 </body>
 </html>
