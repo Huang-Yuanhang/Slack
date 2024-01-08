f8f9c378c067c868c4983662f6a51acaa3a049d7
KantapongWongJC14405427 <128341706+KantapongWongJC14405427@users.noreply.github.com>
2024-01-08 11:25:56 +0800

    added Info slide show

--- version 3/index.php
+++ version 3/index.php
@@ -77,8 +77,9 @@ get_header();
 
         <!-- Info Section -->
         <section class="home-info" id="about">
-            <h2>About JCU</h2>
-            <p>James Cook University has been at the forefront of education and research. Our programs offer a blend of theory and practical experience to prepare you for the real world.</p>
+            <h2 class="about-intro">About JCU</h2>
+            <p class="about-intro">James Cook University has been at the forefront of education and research. 
+                Our programs offer a blend of theory and practical experience to prepare you for the real world.</p>
         
             <!-- About JCU Section -->
             <section id="about" class="about-jcu-section">
@@ -141,6 +142,52 @@ get_header();
                 </div>
             </div>
         </section>
+        
+        <div class="slideshow-container">
+            <!-- First Slide -->
+            <div class="slide fade">
+                <h2>Trimester System</h2>
+                <p>With a trimester program, you can complete your Bachelors degree in 2 years 
+                    and Masters degree in 1 year (depending on the course taken).</p>
+                <h2>Globally recognised Australian degree.</h2>
+                <p>You will graduate with exactly the same globally recognised degree as 
+                    students who study at James Cook University in Australia.</p>
+            </div>
+
+            <!-- Second Slide -->
+            <div class="slide fade">
+                <h2>JCU Intercampus Exchange Program</h2>
+                <p>James Cook University provides the opportunity for you to study in Singapore 
+                    and in Australia and yet still remain as a Singapore based student paying your fees in Singapore currency. 
+                    This is a unique program in Asia and is only able to operate because the university operates across both countries. 
+                    The program is designed to ensure you get the best possible outcomes from your years of study with the university.</p>
+            </div>
+
+            <!-- Third Slide -->
+            <div class="slide fade">
+                <h2>Admissions Requirements</h2>
+                <p>The entry requirements differ from country to country and according to the applicant's desired course of study. 
+                    In general applicants must have satisfactorily completed 12 years of schooling 
+                    and appropriate English Proficiency level equating to a minimum IELTS of 6.0. 
+                    Pathway programs are available for progression to our degree courses.</p>
+                <p>Email: <a href="mailto:admissions-singapore@jcu.edu.au">admissions-singapore@jcu.edu.au</a></p>
+            </div>
+
+            <!-- Fourth Slide -->
+            <div class="slide fade">
+                <h2>Varie of courses</h2>
+                <p><b> majors in Business:</b> You can complete the double major at no extra cost or time commitment</p>
+                <p><b> Joint Masters of IT and Business Administration:</b> Get broader industry exposure and wider career options with a joint Masters program.</p>
+                <p><b>Psychology Accreditation:</b> Our Bachelor of Psychological Science courses are accredited by the Australian Psychology Accreditation Council (APAC).</p>
+                <p><b>Diploma Programs:</b> Students with Year 12, GCE 'A' Level or its equivalent who do not meet direct entry to a degree program are eligible to pursue this qualification.</p>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+
+
 
         <!-- Include form from form.php -->
         <section id="enroll-now-form">
@@ -217,6 +264,30 @@ get_header();
         });
     });
 
+    // Slide show - information admission
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    }
 
 </script>
 
